10bcee3a8f7c1e51f9793f4ad677b818b9240606
Kestutis Januskevicius <infohata@gmail.com>
2023-09-12 10:43:27 +0000

    SSL server side, done

--- init_cert.sh
+++ init_cert.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(gka.lt)
+domains=(gka.lt gerokodoakademija.lt)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="kestas@kpa.lt" # Adding a valid address is strongly recommended
